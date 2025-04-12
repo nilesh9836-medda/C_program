6dfd411e19ffa9795929215f889d14bdb6ccab93
Nilesh Medda <meddanilesh7@gmail.com>
2025-04-13 01:19:13 +0530

    Create sum.c

---
+++ Basic/sum.c
@@ -0,0 +1,14 @@
+/* Add two numbers from input using C program */
+
+#include<stdio.h>
+int main(){
+	int a, b, result;
+	printf("Enter first number: \n");
+	scanf("%d", &a);
+	printf("Enter second number: \n");
+	scanf("%d", &b);
+	result=a+b;
+	printf("Result= %d", result);
+	
+	return 0;
+}
